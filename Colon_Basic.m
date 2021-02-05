b404b1422b47ad58da99ea26837b99dc0d398b31
Iftu119 <iftekheriftu@gmail.com>
2021-02-05 11:24:29 +0600

    Readme and Colon Files

---
+++ Colon_Basic.m
@@ -0,0 +1,16 @@
+
+a = 1:10
+b = 1:2:10
+c = 1:10:100
+d = 100:-10:-50
+
+
+% Use of colon operator
+    
+    %X-axis from 0 to 2pi
+    x = [0:0.1:2*pi]
+    %sine values on Y-axis
+    y = sin(x);
+    %Plotted X and Y values
+    plot(x,y)
+    grid on
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Matlab Learn
+
+All the files for Matlab Learn series will be uploaded in this repository.
