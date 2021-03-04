caf54ee7d505484e51d2d6a09f6412c2ebfc3a67
Iftu119 <iftekheriftu@gmail.com>
2021-03-04 16:54:17 +0600

    Shift

---
+++ Mat_ShiftingOperation.m
@@ -0,0 +1,8 @@
+%Shifting Operation
+
+X = [1:5 ; 6:10 ; 11:15 ; 16:20]
+
+a = circshift(X, [1,2]) % 1 Row 2 Col right
+b = circshift(X, [-1,2])% 1 Row left, 2 Col right
+c = circshift(X, [1,0])
+d = circshift(X, [0,1])
