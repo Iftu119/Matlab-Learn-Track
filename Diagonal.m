eb727615c5ea8e01ec566da8c0c9b5bc478d1f3c
Iftu119 <iftekheriftu@gmail.com>
2021-03-07 22:05:15 +0600

    Diagonals

---
+++ Diagonal.m
@@ -0,0 +1,13 @@
+
+X = [1:5 ; 6:10 ; 11:15 ; 16:20 ; 10:3 :24]
+
+%Finding Diagonal Elements
+b=diag(X)
+c=diag(X,1)
+d=diag(X,-1)
+
+%Creating Diagonal Matrix
+X=[1 26 76 55 ]
+x=diag(X)
+y=diag(X,1)
+z=diag(X,-1)
