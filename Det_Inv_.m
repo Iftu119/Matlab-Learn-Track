f3e5627a68ed72059084ff686f9970652a8938cd
Iftu119 <iftekheriftu@gmail.com>
2021-03-07 21:57:39 +0600

    Inverse and Determinant

---
+++ Det_Inv_.m
@@ -0,0 +1,17 @@
+% Inverse | Determinant
+
+X = [1:2:5 ; 2:4 ; 1 3 4]
+
+%both these commands can calculate inverse of a matrix
+x=inv(X)
+x1=X^-1
+
+Y = [1:2:5 ; 2:4 ]
+% y1=Y^-1
+% y1 - will cause error due to dimensional mismatch
+
+%Determinant
+a=det(X)
+% b=det(Y)
+% b - will cause error due to dimensional mismatch
+
