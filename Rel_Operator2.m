2239dfa0a126464acdbf9ac051c1a7a4215f9369
Iftu119 <iftekheriftu@gmail.com>
2021-03-08 18:15:56 +0600

    Relational Operator -2

---
+++ Rel_Operator2.m
@@ -0,0 +1,19 @@
+%Relational Operation in Matrix Part-2
+A=[1 16 76 15 53 ]
+B=[3 16 61 25 23 ]
+
+m = ne(A,B)
+
+X='Chattogram'
+Y='Chittagong'
+
+a=ne(X,Y)
+
+X1='Hello'
+Y1='Hi there!'
+
+% b=ne(X1,Y1)
+% c=ne(Y1,X1)
+
+X2='h'
+d=ne(Y1,X2)
