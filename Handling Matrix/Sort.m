620ce61b707d23a278213f4ad421668a510c4d23
Iftu119 <iftekheriftu@gmail.com>
2021-03-10 13:21:42 +0600

    Sorting Matrix Elements

---
+++ Handling Matrix/Sort.m
@@ -0,0 +1,16 @@
+%Sorting the Elements of a Matrix
+
+A =rand(1,5)
+X = [1:5 ; 6:10 ; 11:15 ; 16:20 ; 10:3 :24]
+
+p = sort(A)
+p1= sort(X) %Default Sort is by Column in Ascending order
+q1= sort(X,2)%Sorted in Row
+r = sort(A,'descend')%Sorted in descending order
+r1= sort(X,'descend')
+r12 = sort(X,2,'descend')
+[T,u] = sort(X)%Sorted alongside position displayed
+
+%Sorting a String
+B= ["Hello" "are" "You" "There" "Hi"]
+b=sort(B)
