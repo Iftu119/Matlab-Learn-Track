d0ac27c2168926b20d1ce1671ceabef0fe002883
Iftu119 <iftekheriftu@gmail.com>
2021-03-10 13:51:12 +0600

    Concatenate

---
+++ Handling Matrix/Concatenate.m
@@ -0,0 +1,15 @@
+%Concatenate Matrices
+
+X = [1 2 3 ; 4 5 6]
+Y = [7 8 9 ; 5 6 8]
+A=[X;Y] %Row concatenated 
+
+X1 = [1 2 3 ; 4 5 6 ; 9 0 3 ]
+Y1 =[1 2 3 4; 5 6 7 8 ; 8 9 8 7]
+A1=[X1;Y]%Row concatenated 
+A2=[X1 Y1]%Column concatenated
+
+% Row Concat : Same number of Cols, use Semicolon
+% Col Concat : Same number of Rows, use White space
+
+%A12= [X1 Y] %Error due to mismatch
