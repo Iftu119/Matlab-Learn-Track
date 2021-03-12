0cc11159d19cb337bb2830c1f3821c2caea912ae
Iftu119 <iftekheriftu@gmail.com>
2021-03-12 22:53:40 +0600

    Finding Non-Zero Elements

---
+++ Handling Matrix/Find_NonZero.m
@@ -0,0 +1,9 @@
+%Finding Non-Zero Elements
+
+X = [11 0 29 ; 0 51 16 ; 22 19 0]
+
+a = find(X)%Position of Non-Zero Elements
+b = find(X,3) %Position of FIRST 3 Non-Zero Elements
+k = find(X,2,'last') %Position of LAST 2 Non-Zero Elements
+[r,c] = find(X) %Row and Column of Non-Zero Elements
+[row,col,v] = find(X) %Provide a VECTOR of non-zero elements with Row and Column position displayed
