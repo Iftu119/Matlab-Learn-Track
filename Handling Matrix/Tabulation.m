353cd83e9355d000a455613044cc0bddf192f03b
Iftu119 <iftekheriftu@gmail.com>
2021-03-13 18:17:42 +0600

    Frequency Data | Tabulation

---
+++ Handling Matrix/Tabulation.m
@@ -0,0 +1,11 @@
+%Frequency Related Information | Tabulation
+
+X= [ 2 3 1 5 2  3 2 1 4 7 5 4 5 13 0]
+p= tabulate(X)
+
+A = [ 2 3 1 5 ; 2 3 2 1 ; 4 7 5 4 ]
+
+% z=tabulate(A) %Error due to multiple dimensional standing
+q = tabulate(A(1,:))% from 1st row
+r = tabulate (A(:,4)) % from 4th Col
+
