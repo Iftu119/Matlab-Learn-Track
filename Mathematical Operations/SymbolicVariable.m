46aef55f2a83f5049a79350f6a9ce23d3f00ac27
Iftu119 <iftekheriftu@gmail.com>
2021-03-15 23:44:27 +0600

    Symbolic Variable Update

--- Mathematical Operations/SymbolicVariable.m
+++ Mathematical Operations/SymbolicVariable.m
@@ -1,11 +1,25 @@
 %Symbolic Variable
-
-x = sym('x')
-A = sym('a',[n1 ... nM])
-A = sym('a',n)
-sym(___,set)
-sym(___,'clear')
-sym(num)
-sym(num,flag)
-sym(strnum)
-symexpr = sym(h)
+% NEEDS a LOT OF WORK IN COMMAND AREA
+U = sym('m')
+M=5*9+6-2
+V = sym('M1')
+M1 = m*2 +5*m
+% m=4
+% M
+% Y = sym('a',[1 7])
+% YY = sym('a',[2 3])
+% Z = sym('b',2)
+% A = sym (a)
+% 
+% p= (1+sqrt(sym(3)))
+% q= 1+sqrt(3)
+% 
+% syms a x b c
+% y = sym(a*x^2 +b*x +c)
+% 
+% sym(___,set)
+% sym(___,'clear')
+% sym(num)
+% sym(num,flag)
+% sym(strnum)
+% symexpr = sym(h)
