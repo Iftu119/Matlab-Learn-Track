9464551182cc4e34a9ab6dd47f921823d3ff818c
Iftu119 <iftekheriftu@gmail.com>
2021-03-18 12:45:42 +0600

    Eqn-3 | Substitution & Coefficient

---
+++ Mathematical Operations/EqnSolve3.m
@@ -0,0 +1,16 @@
+%Equation Solve (Part-3)
+syms x y z
+
+%Substitution
+A= 10*x*x - 10*x + 2
+ansA = subs(A,5)
+Axy= 10*x*x - 10*y + 2
+ansAxy = subs(Axy,5)
+ansAxy = subs(Axy,y,5)
+Ay = subs(A,x,y)
+
+% Coeffiecient
+p = sym2poly(A)
+
+B= 10*x^4 + 13*x^2 + 5*x 
+q = sym2poly(B) %Zero for the missing components
