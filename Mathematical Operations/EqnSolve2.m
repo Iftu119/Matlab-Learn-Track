30679db0a62381fd41abd2b6ea7a40fd3acf99dc
Iftu119 <iftekheriftu@gmail.com>
2021-03-18 12:45:09 +0600

    Eqn-2 | Multivariate & System of Eqn

---
+++ Mathematical Operations/EqnSolve2.m
@@ -0,0 +1,18 @@
+%Equation Solve (Part-2)
+
+syms x y z
+
+% Multivariate Equation
+
+f = [5*x+2*y==5 , 2*x+2*y==7]
+u = solve(f,[x y])
+u1 = u.x
+u2 = u.y
+
+% System of Equations
+
+f1= 3*x==z
+f2= -y+x==0
+f3= y^2-z+5*x==0
+
+[x, y, z] = solve (f1,f2,f3)
