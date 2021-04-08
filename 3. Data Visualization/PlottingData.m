380f46797ac9625d92fd8c1e8796251087868d09
Iftu119 <iftekheriftu@gmail.com>
2021-04-08 11:03:19 +0600

    Quick fix

--- 3. Data Visualization/PlottingData.m
+++ 3. Data Visualization/PlottingData.m
@@ -1,5 +1,14 @@
-x = [ 1 2 6  3 8 7 ]
-y = [ 7 6 6  3 5 7 ]
-plot(x,y)
+X = [ 1 2 6  3 8 7 ]
+Y = [ 7 6 6  3 5 7 ]
+% plot(X,Y)
 % plot(x);
 % plot(y);
+
+
+plot(X,Y,'-sr')
+% plot(X1,Y1,...,Xn,Yn)
+% plot(X1,Y1,LineSpec1,...,Xn,Yn,LineSpecn)
+% plot(Y)
+% plot(Y,LineSpec)
+% plot(___,Name,Value)
+% plot(ax,___)
