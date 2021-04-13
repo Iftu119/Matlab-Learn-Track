94bff3847e0b5696daeb65a0d374336cf22c8958
Mohammad Iftekher Ebne Jalal <iftekheriftu@gmail.com>
2021-04-13 07:56:03 +0600

    Change in 2.A.9 Randomization

--- 2.Mathematical Operations/2.A  Basic/2.A.9 Random.m
+++ 2.Mathematical Operations/2.A  Basic/2.A.9 Random.m
@@ -1,12 +1,18 @@
+%Randomization
 
+a=rand (3) %Random 3*3 Matrix
+b=rand (3,2) %Random 3*2 Matrix
 
-rand (3)
-rand (3,2)
+c=randn (3) %Normal Distribution Random 3*3 Matrix
+c1=randn (3,2) %Normal Distribution Random 3*2 Matrix
 
-randn (3)
+d=randperm (15) %Values from 1 to 15
+e=randperm (15,5) %Any 5 Values from 1 to 15
 
-randperm (15)
+f = 3+randperm (15) % Values from (1+3=)4 to (15+3=)18
+g = 2+randperm (13,5) % 5 Values from (1+2=)3 to (13+2=)15
 
-randperm (15,5)
-
-p = 3+randperm (15)
+h=randi(4)%Any value from 1 to 4
+i=randi(4,3)% 3*3 Matrix with values from 1 to 4
+i=randi(4,3,2) % 3*2 Matrix with values from 1 to 4
+i=1+randi(4,3,2)%3*2 Matrix with values from (1+1=)2 to (4+1=)5
