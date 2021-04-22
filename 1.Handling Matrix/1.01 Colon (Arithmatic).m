e19b7d29f6a36cae2be851af4ff9cda38181268c
Mohammad Iftekher Ebne Jalal <iftekheriftu@gmail.com>
2021-04-22 20:20:08 +0600

    Video Optimized 1.01

---
+++ 1.Handling Matrix/1.01 Colon (Arithmatic).m
@@ -0,0 +1,11 @@
+% Handling Matrix
+% Colon Operator with Numeric Data Type
+% Video Link : youtu.be/F_in38ntn_w
+
+a = 1:10         %Prints 1 to 10 
+b = 1:2:10       %Prints 1 to 10 with +2 Step/Increment
+c = 1:25:100000
+% d=0.5:0.8      %Does not work due to stepping ambiguity
+d = 0.5:0.1:0.8  %float number with float increment
+d1 = 1:0.1:2     %Integer with float increment
+e = -1:-1:-5     %Decrement
--- 1.Handling Matrix/1.01 Colon_Basic.m
+++
@@ -1,9 +0,0 @@
-%Colon Operator with Numeric Data Type
-
-a = 1:10 %Prints 1 to 10 
-b = 1:2:10 %Prints 1 to 10 with +2 Step/Increment
-c = 1:25:100000
-% d=0.5:0.8 %Does not work due to stepping ambiguity
-d = 0.5:0.1:0.8 %float number with float increment
-d1 = 1:0.1:2 %Integer with float increment
-e = -1:-1:-5 %Decrement
