41f5143bf3965b256628f684f8272e2a882e6b35
Mohammad Iftekher Ebne Jalal <iftekheriftu@gmail.com>
2021-04-25 10:00:50 +0600

    Update 1.10 Rel_Operator2.m

--- 1.Handling Matrix/1.10 Rel_Operator2.m
+++ 1.Handling Matrix/1.10 Rel_Operator2.m
@@ -1,4 +1,6 @@
 %Relational Operation in Matrix Part-2
+%Video Link : https://youtu.be/ejjbf4Il_kA
+
 A=[1 16 76 15 53 ]
 B=[3 16 61 25 23 ]
 
@@ -14,6 +16,7 @@ Y1='Hi there!'
 
 % b=ne(X1,Y1)
 % c=ne(Y1,X1)
+%Above 2 commands have dimensional mismatch
 
 X2='h'
 d=ne(Y1,X2)
