7c19ae6f1e3ad296a92a34510b7bf947b7dc7b98
Mohammad Iftekher Ebne Jalal <iftekheriftu@gmail.com>
2021-04-25 10:26:15 +0600

    Update 1.13 Concatenate.m

--- 1.Handling Matrix/1.13 Concatenate.m
+++ 1.Handling Matrix/1.13 Concatenate.m
@@ -1,4 +1,5 @@
-%Concatenate Matrices
+% Concatenate Matrices
+% Video : https://youtu.be/3RJ7Ko1Ct3A
 
 X = [1 2 3 ; 4 5 6]
 Y = [7 8 9 ; 5 6 8]
@@ -6,10 +7,13 @@ A=[X;Y] %Row concatenated
 
 X1 = [1 2 3 ; 4 5 6 ; 9 0 3 ]
 Y1 =[1 2 3 4; 5 6 7 8 ; 8 9 8 7]
-A1=[X1;Y]%Row concatenated 
-A2=[X1 Y1]%Column concatenated
+A1=[X1;Y]   % Row concatenated 
+A2=[X1 Y1]  % Column concatenated
 
 % Row Concat : Same number of Cols, use Semicolon
 % Col Concat : Same number of Rows, use White space
 
 %A12= [X1 Y] %Error due to mismatch
+
+%% Task
+% Write a string with concatenating multiple elements
