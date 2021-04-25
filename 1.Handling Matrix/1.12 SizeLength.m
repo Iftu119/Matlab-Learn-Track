99a542d6c01b44df9919523946ef56722873d719
Mohammad Iftekher Ebne Jalal <iftekheriftu@gmail.com>
2021-04-25 15:32:26 +0600

    Update 1.12 SizeLength.m

--- 1.Handling Matrix/1.12 SizeLength.m
+++ 1.Handling Matrix/1.12 SizeLength.m
@@ -1,4 +1,5 @@
-%Size and Length
+% Size and Length
+% Video Link : https://youtu.be/YpU0kh12CPk
 
 X = [1:5 ; 6:10 ; 16:20 ; 10:3 :24]
 a =size(X)
@@ -7,7 +8,7 @@ b = length(X)
 %Length only presents the maximum number of elements in either row or column whichever is max
 
 %Size with String
-Y = ["Hello" "are" "You" "There" "Hi"]
+Y = ["Hello" "are" "You" "There" "Hi"] %This command may not work in some of the earlier versions of Matlab
 p = size(Y)
 q = length(Y)
 
