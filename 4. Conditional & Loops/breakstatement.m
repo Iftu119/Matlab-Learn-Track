30d6f58f249c76fe46ed8c35d73223915fd6cac3
Iftu119 <iftekheriftu@gmail.com>
2021-04-29 05:50:31 +0600

    Break Statement Added

---
+++ 4. Conditional & Loops/breakstatement.m
@@ -0,0 +1,12 @@
+%% Break
+p=input('Enter a number : ')
+for i=p:p+10
+    fprintf ('Number : %d \n',i)
+    if (mod(i,12)==0)
+        disp ('This is divisible by 12. We are ending here.')
+    break
+    end 
+    disp ('Successful')
+end
+
+disp ('Break demonstrated')
