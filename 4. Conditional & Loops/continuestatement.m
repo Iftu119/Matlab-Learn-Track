04ca23ccaae32f852a6e5561b55f8d5bfc955a71
Iftu119 <iftekheriftu@gmail.com>
2021-04-29 05:50:57 +0600

    Continue Statement Added

---
+++ 4. Conditional & Loops/continuestatement.m
@@ -0,0 +1,32 @@
+%% Continue
+p=input('Enter a number : ')
+for i=p:p+10
+    fprintf ('Number : %d \n',i)
+    if (mod(i,12)==0)
+        disp ('Fail : This is divisible by 12.')
+    continue
+    end 
+    disp ('Successful')
+end
+
+disp ('Continue demonstrated')
+
+
+%% Example - Data of Students
+clc
+clear all
+disp(' *** EXAM-01 ***')
+a=input('How many students are there in the class : ');
+for i=1:a
+    
+    fprintf ('Enter Data for Roll : %d \n',i)
+    fprintf ('Did he appear ? If Yes, enter Y else enter N\n')
+    c=input(' ', 's');
+
+    if c == 'N'
+        disp ('Avoided')
+    continue
+    end 
+    marks = input ('Enter his score :');
+    disp ('Noted')
+end
